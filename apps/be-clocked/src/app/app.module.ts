1b99f8298c92a8e18789b467c31241feef4ded83
Aleksandr Koltovich <avkoltovich@gmail.com>
2022-07-22 13:37:41 +0300

    feat: footer module

--- apps/be-clocked/src/app/app.module.ts
+++ apps/be-clocked/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { HeaderModule } from "./header/header.module";
+import { FooterModule } from "./footer/footer.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, HeaderModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, AppRoutingModule, HeaderModule, FooterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
