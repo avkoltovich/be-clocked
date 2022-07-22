acce0d63622a1b1ee9cff5ed64262f94ffc14bfe
Aleksandr Koltovich <avkoltovich@gmail.com>
2022-07-22 12:34:17 +0300

    feat: Material

--- apps/be-clocked/src/app/app.component.ts
+++ apps/be-clocked/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Message} from '@be-clocked/api-interfaces';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'be-clocked-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "be-clocked-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  constructor() {
+  }
 }
