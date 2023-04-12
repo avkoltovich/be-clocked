66e2d4748e67dfbae7bd61827f0455e954ec7aea
Aleksandr Koltovich <avkoltovich@gmail.com>
2023-04-12 11:32:10 +0300

    feat: Удаление старой версии и установка свежего Ангуляра

--- shared/constants.ts
+++
@@ -1,6 +0,0 @@
-export const JWT_TOKEN = "JWT_TOKEN";
-export const USER_EMAIL = "USER_EMAIL";
-export const USER_ID = "USER_ID";
-
-export const API_URL = "https://be-clocked-api.herokuapp.com/api";
-export const APP_URL = "https://be-clocked.web.app/";
